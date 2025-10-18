2c0e59553f4ccaf52814c8ce9d0444e4966a1194
zvinorez <cebepeh@gmail.com>
2025-10-18 20:21:48 +0400

    ,

--- tasks/Task3.h
+++ tasks/Task3.h
@@ -20,18 +20,18 @@ public:
     }
 
 private:
-    FuncParams getUserInput() {
+    FuncParams getUserInput() const {
         FuncParams p;
         std::cout << "Enter value X, A and B\n";
         std::cin >> p.x >> p.a >> p.b;
         return p;
     }
 
-    double calculateY(FuncParams p) {
+    double calculateY(FuncParams p) const {
         return p.b * (1 - std::exp(-0.5 * p.a)) * std::cos(2 * M_PI * p.x);
     }
 
-    void printResult(FuncParams p, double y) {
+    void printResult(FuncParams p, double y) const {
         std::cout << "For input data: " << p.x << ", " << p.a << ", " << p.b << '\n';
         std::cout << "Result: Y = " << y << '\n';
     }
